432e9b968d90b541ef0027548a2eb988c0786935
Daniel Strizhevsky <danielstrizhevsky@gmail.com>
2015-10-04 22:16:24 -0500

    Updated readme and cleaned up calibrate.py

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Usage:
+
 Run calibrate.py to start calibrating the Sensel based on the prompt, and the begin typing.
 'saved' holds the state of the saved calibration so that it may be reused.
+Set variable load_grid to True if you want to load from saved grid.
+Set variable save_grid to True if you want to save (overwrite) your next calibration.
+Uncomment 'show_plot()' if you want to see a heatmap of your keyboard.
+This will crash the Sensel Morph if you don't quickly close the graph.
--- calibrate.py
+++ calibrate.py
@@ -1,31 +1,5 @@
 #!/usr/bin/env python
 
-##########################################################################
-# Copyright 2015 Sensel, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##########################################################################
-
-#
-#  Read Contacts
-#  by Aaron Zarraga - Sensel, Inc
-#
-#  This opens a Sensel sensor, reads contact data, and prints the data to the console.
-#
-#  Note: We have to use \r\n explicitly for print endings because the keyboard reading code
-#        needs to set the terminal to "raw" mode.
-##
-
 from pylab import *
 from keyboard_reader import *
 import sensel
